7442592cbff2b0b273795eaf3db981cf84e744dd
Tom Bowden <tom.bowden@gmail.com>
2019-03-06 16:52:19 +0900

    Added files after pushing to AWS cloud

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,27 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = `subscription OnCreateTodo {
+  onCreateTodo {
+    id
+    text
+    completed
+  }
+}
+`;
+export const onUpdateTodo = `subscription OnUpdateTodo {
+  onUpdateTodo {
+    id
+    text
+    completed
+  }
+}
+`;
+export const onDeleteTodo = `subscription OnDeleteTodo {
+  onDeleteTodo {
+    id
+    text
+    completed
+  }
+}
+`;
