4beed9f87c6091ffa7bc61da123ce163a02319f5
Tom Bowden <tom.bowden@gmail.com>
2019-03-06 17:42:26 +0900

    Update App and index.js with Amplify

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,116 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { generate } from 'shortid'
+import { API, graphqlOperation } from 'aws-amplify'
 
 import TodoItem from './TodoItem'
 import TodoTextInput from './TodoTextInput'
-import { initialTodos } from '../data'
+import { listTodos } from '../graphql/queries'
+import { createTodo, deleteTodo, updateTodo } from '../graphql/mutations'
 
 const App = () => {
-  const [todos, setTodos] = useState(initialTodos)
+  const [todos, setTodos] = useState([])
 
-  const addTodo = todoText => {
+  const fetchTodos = async () => {
+    try {
+      const response = await API.graphql(graphqlOperation(listTodos))
+      // Note: todos are under listTodos.items in this schema...
+      const fetchedTodos = response.data.listTodos.items
+      setTodos(fetchedTodos)
+    } catch (err) {
+      console.log('Error in fetchTodos:', err)
+    }
+  }
+
+  // Fetch todos on mount
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  const addTodo = async todoText => {
+    // Update UI immediately
     const addedTodo = {
       id: generate(),
-      completed: false,
       text: todoText,
+      completed: false,
     }
-    const updateTodos = [...todos, addedTodo]
-    setTodos(updateTodos)
+    const updatedTodos = [...todos, addedTodo]
+    setTodos(updatedTodos)
+    // Send operation to the API
+    try {
+      const createTodoInput = {
+        id: addedTodo.id,
+        text: addedTodo.text,
+        completed: addedTodo.completed,
+      }
+      await API.graphql(
+        graphqlOperation(createTodo, { input: createTodoInput })
+      )
+    } catch (err) {
+      console.log(`Error in addTodo API call with text ${todoText}: ${err}`)
+    }
   }
 
-  const removeTodo = todoId => {
-    const updateTodos = todos.filter(todo => todo.id !== todoId)
-    setTodos(updateTodos)
+  const removeTodo = async todoId => {
+    // Update UI immediately
+    const updatedTodos = todos.filter(todo => todo.id !== todoId)
+    setTodos(updatedTodos)
+    // Send operation to the API
+    try {
+      const deleteTodoInput = {
+        id: todoId,
+      }
+      await API.graphql(
+        graphqlOperation(deleteTodo, { input: deleteTodoInput })
+      )
+    } catch (err) {
+      console.log(`Error in removeTodo API call with id ${todoId}: ${err}`)
+    }
   }
 
-  const editTodo = (todoId, todoText) => {
+  const editTodo = async (todoId, todoText) => {
+    // Update UI immediately
     const updateTodos = [...todos]
     const index = updateTodos.findIndex(todo => todo.id === todoId)
     const editedTodo = updateTodos[index]
     editedTodo.text = todoText
     setTodos(updateTodos)
+    // Send operation to the API
+    try {
+      const updateTodoInput = {
+        id: todoId,
+        text: todoText,
+        completed: editedTodo.completed,
+      }
+      await API.graphql(
+        graphqlOperation(updateTodo, { input: updateTodoInput })
+      )
+    } catch (err) {
+      console.log(
+        `Error in editTodo API call with id ${todoId} and text ${todoText}: ${err}`
+      )
+    }
   }
 
-  const completeTodo = todoId => {
+  const completeTodo = async todoId => {
+    // Update UI immediately
     const updateTodos = [...todos]
     const index = updateTodos.findIndex(todo => todo.id === todoId)
     const editedTodo = updateTodos[index]
     editedTodo.completed = !editedTodo.completed
     setTodos(updateTodos)
+    // Send operation to the API
+    try {
+      const updateTodoInput = {
+        id: todoId,
+        text: editedTodo.text,
+        completed: editedTodo.completed,
+      }
+      await API.graphql(
+        graphqlOperation(updateTodo, { input: updateTodoInput })
+      )
+    } catch (err) {
+      console.log(`Error in completeTodo API call with id ${todoId}: ${err}`)
+    }
   }
 
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Amplify from 'aws-amplify'
 
 import App from './components/App'
 import 'todomvc-app-css/index.css'
+import aws_exports from './aws-exports'
+
+Amplify.configure(aws_exports)
 
 ReactDOM.render(<App />, document.getElementById('root'))
